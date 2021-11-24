743134e703bf6687da752856d81358bd470888ba
Samir Nacer <nacer.smr@gmail.com>
2021-11-25 00:38:58 +0100

    recup film+img

--- test/indexTest.php
+++ test/indexTest.php
@@ -1,35 +1,35 @@
-<?php include("/sources/config.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Getflix Home</title>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-</body>
-</html>
+<?php
+include("/sources/config.php");
+
+$url = "https://api.themoviedb.org/3/discover/movie?api_key=0374b79f54a000c4b81f9c12db4437df&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate";
+//$url2= "https://api.themoviedb.org/3/discover/movie?api_key=0374b79f54a000c4b81f9c12db4437df&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate";
+//$url3=https://api.themoviedb.org/3/movie/latest?api_key=0374b79f54a000c4b81f9c12db4437df&language=en-US
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$movies = curl_exec($curl);
+curl_close($curl);
+$response = json_decode($movies);
+
+echo '<pre>';
+print_r($response);
+echo '</pre>';
+$films = $response->results;
+$countFilms = count($films);
+
+/*
+for ($i = 0; $i < $countFilms; $i++) {
+  $titleMovie[$i] = $films[$i]->title;
+  $overview[$i] = $films[$i]->overview;
+  $picture[$i] = $films[$i]->backdrop_path;
+  $urlImg = "https://image.tmdb.org/t/p/w185" . $picture[$i];
+
+?>
+
+  <img src="<?php print($urlImg) ?>">
+<?php
+}
+*/
+?>
